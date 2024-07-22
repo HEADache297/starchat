fa2e3e6121f81e9f3f0f3e34da34baa929a08f6a
Nazar <nazarsvistula11@gmail.com>
2024-07-23 00:07:33 +0300

    Form styles

--- user/forms.py
+++ user/forms.py
@@ -24,17 +24,21 @@ class ChangeImageForm(forms.Form):
 
 
 class UpdatePasswordForm(SetPasswordForm):
+    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-white text-black', 'placeholder': 'Password', 'id': 'inp1'}))
+    new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-white text-black', 'placeholder': 'Password Confirmation', 'id': 'inp2'}))
+
+    def __init__(self, *args, **kwargs):
+        super(UpdatePasswordForm, self).__init__(*args, **kwargs)
+        self.fields['new_password1'].label = ""
+        self.fields['new_password2'].label = ""
+
     class Meta:
         model=User
         fields= ['new_password1', 'new_password2']
-        widgets = {
-            'new_password1': forms.PasswordInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-white text-black', 'placeholder': 'Password'}),
-            'new_password2': forms.PasswordInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-white text-black', 'placeholder': 'Password Confirmation'}),
-        }
 
 class ResetPasswordForm(PasswordResetForm):
-    username=forms.CharField(required=True, min_length=3, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-white text-black', 'placeholder': 'Username'}))
-    email=forms.EmailField(required=True, widget=EmailInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-white text-black', 'placeholder': 'Email'}))
+    username=forms.CharField(required=True, min_length=3, widget=Input(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-white text-black', 'placeholder': 'Username', 'id': 'inp1'}))
+    email=forms.EmailField(required=True, widget=EmailInput(attrs={'class': 'm-3 h-12 rounded-lg p-2 bg-white text-black', 'placeholder': 'Email', 'id': 'inp2'}))
     
     def __init__(self, *args, **kwargs):
         super(ResetPasswordForm, self).__init__(*args, **kwargs)
